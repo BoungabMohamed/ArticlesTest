276c32cb5a694ac7e6d551598da1d879f9f4f97a
MOHAMMED AMIR BOUNGAB <boungab.mohammedamir@univ-ouargla.dz>
2024-03-25 05:17:15 -0300

    Add Editor js

---
+++ Trach/Utility/ContentBlock.cs
@@ -0,0 +1,8 @@
+﻿namespace Trach.Utility
+{
+    public class ContentBlock
+    {
+        public string Type { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ Trach/Utility/JsonC.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.Json;
+
+namespace Trach.Utility
+{
+
+    public class JsonC
+    {
+        public static List<ContentBlock> GetContentBlocks(string jsonContent)
+        {
+            var blocks = new List<ContentBlock>();
+
+            try
+            {
+                var jsonDocument = JsonDocument.Parse(jsonContent);
+
+                if (jsonDocument.RootElement.TryGetProperty("blocks", out var blocksElement) && blocksElement.ValueKind == JsonValueKind.Array)
+                {
+                    foreach (var block in blocksElement.EnumerateArray())
+                    {
+                        var type = block.TryGetProperty("type", out var typeElement) ? typeElement.GetString() : null;
+                        var content = block.TryGetProperty("content", out var contentElement) ? contentElement.GetString() : null;
+
+                        if (type != null && content != null)
+                        {
+                            blocks.Add(new ContentBlock
+                            {
+                                Type = type,
+                                Content = content
+                            });
+                        }
+                    }
+                }
+            }
+            catch (JsonException) // In case of parsing errors
+            {
+                // Handle the error as appropriate for your application
+            }
+
+            return blocks;
+        }
+
+    }
+}
